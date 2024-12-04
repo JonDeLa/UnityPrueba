a490fef2b8be79969fb551384ede715091402bf6
JonDeLa <91682790+JonDeLa@users.noreply.github.com>
2024-12-04 18:09:03 +0100

    Add files via upload

---
+++ Sample_script.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Sample_script : MonoBehaviour
+{
+    void Start()
+    {
+        print("Pues no te saludo");
+    }
+    void Update()
+    {
+        print(9999);
+    }
+}
